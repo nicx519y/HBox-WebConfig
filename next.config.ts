9faef2328e3724f107dc382eff72c2bb483b5c03
nicx519y <nicx519y@gmail.com>
2024-12-08 01:05:04 +0800

    build release pass

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import compression from "compression";
+import type { Express } from 'express';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async serverMiddleware({ app }: { app: Express }) {
+        app.use(
+            compression({
+                level: 6,
+            })
+        );
+    },
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
     },
