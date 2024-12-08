0302a30b2b612a1012e4c37392d81ebe578e1b53
nicx519y <nicx519y@gmail.com>
2024-12-09 03:06:19 +0800

    static export

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,26 @@ const nextConfig: NextConfig = {
     },
     distDir: ".dist",
     output: "export", // for static export
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.optimization.splitChunks = {
+                chunks: 'all',
+                minSize: 10000,
+                minChunks: 1,
+                maxAsyncRequests: 2,
+                maxInitialRequests: 2,
+            }
+        } else {
+            config.optimization.splitChunks = {
+                chunks: 'all',
+                minSize: 10000,
+                minChunks: 1,
+                maxAsyncRequests: 2,
+                maxInitialRequests: 2,
+            }
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
